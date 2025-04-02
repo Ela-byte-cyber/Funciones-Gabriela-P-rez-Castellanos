1e59b19b134674887eebf2b48a4ef24d3c8806aa
Ela-byte-cyber <gabrielaperezcastellanos@gmail.com>
2025-04-02 08:28:37 -0500

    FuncionGabrielaPérez

---
+++ FuncionGabrielaPerez.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.funcionangieegalvisygabriellaperez;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class FuncionAngieeGalvisYGabriellaPerez {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado=suma(84,99);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b){
+    return a + b;
+}            
+}
